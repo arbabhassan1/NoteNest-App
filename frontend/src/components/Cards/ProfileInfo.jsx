cf446225123fb42194547bb6cb804f2b106e3057
arbabhassan1 <linkto898@hotmail.com>
2024-05-18 08:11:01 -0700

    NAvbar

---
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileInfo = () => {
+  return <div>hello</div>;
+};
+
+export default ProfileInfo;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import ProfileInfo from "./Cards/ProfileInfo";
 const Navbar = () => {
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium py-2 text-black">Notes App</h2>
+      <ProfileInfo />
     </div>
   );
 };
